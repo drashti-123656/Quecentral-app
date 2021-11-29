5596bd33a4c7aff98b88121cb8d841970682ef2d
manishchandra-resiliencesoft <manishchandra@resiliencesoft.com>
2021-11-29 11:03:25 +0530

    changed domain

--- src/utils/global.js
+++ src/utils/global.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://servicefinder.worksdemo.co.in/';
-export const API_URL = 'http://servicefinder.worksdemo.co.in/api/';
+export const BASE_URL = 'http://queuecentral.com/';
+export const API_URL = 'http://queuecentral.com/api/';
