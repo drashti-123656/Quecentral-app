40004f1f02cc40d2ea77912b6613d23688e16b9f
drashti shah <drashtishah@gmail.com>
2022-03-04 20:25:25 +0530

    dropdown show on price, sortby and categories

--- src/screens/FindAProfessional.js
+++ src/screens/FindAProfessional.js
@@ -9,6 +9,8 @@ import {
   TextInput,
   ActivityIndicator,
   Dimensions,
+  Button,
+  Pressable
 } from 'react-native';
 import CategoriesPicker from './../components/picker/CategoriesPicker';
 import Picker from './../components/picker/Picker';
@@ -17,6 +19,7 @@ import {CustomInputWithTitle} from './../components/input/CustomInput';
 import ServiceCard from './../components/cards/ServiceCard';
 import {categoryList, searchService as searchServiceAPI} from '../services/api';
 import NoResultFound from './../components/molecules/NoResultFound';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 import SliderScreen from './../components/Slider/Slider';
 
@@ -31,7 +34,7 @@ const FindAProfessional = ({route, navigation}) => {
   const [location, setLocation] = useState('');
   const [minPrice, setMinPrice] = useState(0);
   const [maxPrice, setMaxPrice] = useState(50);
-
+  const [shouldShow, setShouldShow] = useState(false);
   useEffect(() => {
     searchHandler();
   }, [Categories, sortBy, searchText, minPrice, maxPrice]);
@@ -57,119 +60,127 @@ const FindAProfessional = ({route, navigation}) => {
   };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.headerBar}>
-        <Text style={{...styles.h1, color: '#fff', marginLeft: 10}}>
-          Search Service
-        </Text>
-      </View>
-      <View style={styles.bodyContainer}>
-        <View
-          style={styles.mainContainer}>
-          <ScrollView
-            showsVerticalScrollIndicator={true}
-            contentContainerStyle={{zIndex: 1}}>
-            <View style={styles.filterOptionsCont}>
-              <View
-                style={styles.searchDevice}>
-                <TextInput
-                  style={styles.searchTitle}
-                  value={searchText}
-                  onChangeText={setSearchText}
-                  placeholder="Search Service"
-                  placeholderTextColor="#a1a1a1"
-                />
-
-                <TouchableOpacity onPress={searchHandler}>
-                  <Image
-                    source={require('./../assets/icons/search.png')}
-                    style={styles.serchIcon}
-                  />
-                </TouchableOpacity>
-              </View>
-              <View style={styles.locationTitle}>
-                <CustomInputWithTitle
-                  title={'Location'}
-                  placeholder={'Enter location'}
-                />
-              </View>
-              <View
-                style={{
-                  ...styles.rowCont,
-                  justifyContent: 'space-between',
-                  marginBottom: 5,
-                }}>
-                <View style={styles.sortbyTitle}>
-                  <Picker
-                    title={'Sort By'}
-                    value={sortBy}
-                    data={[
-                      {id: 1, value: 'Price low to high'},
-                      {id: 2, value: 'Price high to low'},
-                      {id: 3, value: 'Newest'},
-                    ]}
-                    onSelect={setSortBy}
-                  />
-                </View>
-
-                <View style={styles.sortbyTitle}>
-                  <CategoriesPicker
-                    title={'Categories'}
-                    value={Categories}
-                    onSelect={setCategories}
-                  />
-                </View>
-              </View>
-
-             
-
-              <View>
-                <Text style={{...styles.h3, color: '#000', marginBottom: 10}}>
-                  Price Range
-                </Text>
-                <SliderScreen
-                  setMinPrice={setMinPrice}
-                  setMaxPrice={setMaxPrice}
-                />
-                <Text
-                  style={{
-                    ...styles.h2,
-                    marginTop: 10,
-                  }}>{`\u20B9 ${minPrice} -  \u20B9 ${maxPrice}`}</Text>
-              </View>
-            </View>
-
-            <View style={styles.searchContainer}>
-              <View
-                style={styles.searchResutcontainer}>
-                <Text style={styles.h2}>Search results</Text>
-              </View>
-
-              {loading ? (
-                <ActivityIndicator color={COLORS.PRIMARY} />
-              ) : (
-                <View>
-                  {searchresultData.length == 0 && <NoResultFound />}
-                  {searchresultData.map((item, i) => (
-                    <View key={i} style={styles.serviceCard}>
-                      <ServiceCard
-                        service_id={item.service_id}
-                        location={item.service_location}
-                        image={item.service_image}
-                        service_title={item.service_title}
-                        mobileno={item.mobileno}
-                        service_amount={item.service_amount}
-                        currency={item.currency}
-                      />
-                    </View>
-                  ))}
-                </View>
-              )}
-            </View>
-          </ScrollView>
+    <><View style={styles.container}>
+        <View style={styles.headerBar}>
+          <Text style={{ ...styles.h1, color: '#fff', marginLeft: 10 }}>
+            Search Service
+          </Text>
         </View>
-      </View>
-    </View>
+        
+        <View style={styles.bodyContainer}>
+          <View
+            style={styles.mainContainer}>
+            <ScrollView
+              showsVerticalScrollIndicator={true}
+              contentContainerStyle={{ zIndex: 1 }}>
+              <View style={styles.filterOptionsCont}>
+             
+                <View
+                  style={styles.searchDevice}>
+                  <TextInput
+                    style={styles.searchTitle}
+                    value={searchText}
+                    onChangeText={setSearchText}
+                    placeholder="Search Service"
+                    placeholderTextColor="#a1a1a1" />
+
+                  <TouchableOpacity onPress={searchHandler}>
+                    <Image
+                      source={require('./../assets/icons/search.png')}
+                      style={styles.serchIcon} />
+                  </TouchableOpacity>
+                </View>
+                <View style={styles.locationTitle}>
+                  <CustomInputWithTitle
+                    title={'Location'}
+                    placeholder={'Enter location'} />
+                </View>
+                <Pressable style={styles.openButton} onPress={() => setShouldShow(!shouldShow)}>
+      <Text  style={styles.text}>OPEN</Text>
+    </Pressable>
+                
+                <View
+                  style={{
+                    ...styles.rowCont,
+                    justifyContent: 'space-between',
+                    marginBottom: 5,
+                    marginTop:15
+                  }}>
+                    
+                  <View style={styles.sortbyTitle}>
+                  {shouldShow ? (
+                    <Picker
+                      title={'Sort By'}
+                      value={sortBy}
+                      data={[
+                        { id: 1, value: 'Price low to high' },
+                        { id: 2, value: 'Price high to low' },
+                        { id: 3, value: 'Newest' },
+                      ]}
+                      onSelect={setSortBy} />
+                      )  : null}
+                  
+                  </View>
+
+                  <View style={styles.sortbyTitle}>
+                  {shouldShow ? (
+                    <CategoriesPicker
+                      title={'Categories'}
+                      value={Categories}
+                      onSelect={setCategories} />
+                      )  : null}
+                  </View>
+                  
+                </View>
+
+
+
+                <View>
+                {shouldShow ? (
+                  <><Text style={{ ...styles.h3, color: '#000', marginBottom: 10 }}>
+                    Price Range
+                  </Text><SliderScreen
+                      setMinPrice={setMinPrice}
+                      setMaxPrice={setMaxPrice} /><Text
+                        style={{
+                          ...styles.h2,
+                          marginTop: 10,
+                        }}>{`\u20B9 ${minPrice} -  \u20B9 ${maxPrice}`}</Text></>
+                    )  : null}
+                </View>
+            
+              </View>
+
+              <View style={styles.searchContainer}>
+                <View
+                  style={styles.searchResutcontainer}>
+                  <Text style={styles.h2}>Search results</Text>
+                </View>
+
+                {loading ? (
+                  <ActivityIndicator color={COLORS.PRIMARY} />
+                ) : (
+                  <View>
+                    {searchresultData.length == 0 && <NoResultFound />}
+                    {searchresultData.map((item, i) => (
+                      <View key={i} style={styles.serviceCard}>
+                        <ServiceCard
+                          service_id={item.service_id}
+                          location={item.service_location}
+                          image={item.service_image}
+                          service_title={item.service_title}
+                          mobileno={item.mobileno}
+                          service_amount={item.service_amount}
+                          currency={item.currency} />
+                      </View>
+                    ))}
+                  </View>
+                )}
+              </View>
+            </ScrollView>
+          </View>
+        </View>
+      </View></>
   );
 };
 
@@ -180,12 +191,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: COLORS.PRIMARY,
   },
+
   mainContainer:{
     position: 'absolute',
     top: -30,
     left: 0,
     right: 0,
     height: Dimensions.get('window').height - 20,
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
   },
   headerBar: {
     height: 50,
@@ -195,6 +214,18 @@ const styles = StyleSheet.create({
     flex: 0.94,
     backgroundColor: '#fff',
     marginTop: 'auto',
+  },
+  openButton:{
+alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 5,
+    paddingHorizontal: 5,
+    marginHorizontal:125,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: COLORS.PRIMARY,
+    width:'30%',
+    marginTop:20
   },
   h1: {
     fontSize: 20,
@@ -254,6 +285,7 @@ const styles = StyleSheet.create({
     zIndex: 1,
     marginBottom: 10,
   },
+  
   input: {
     flex: 1,
     height: 40,
